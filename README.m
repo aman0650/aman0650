8dca8f83df1de35ce4a686142c7e6507af01678c
aman0650 <142194589+aman0650@users.noreply.github.com>
2023-08-14 09:10:31 +0530

    Create README.m

---
+++ README.m
@@ -0,0 +1,10 @@
+- 👋 Hi, I’m @aman0650
+- 👀 I’m interested in web dev
+- 🌱 I’m currently learning threejs
+- 💞️ I’m looking to collaborate on 3d web
+- 📫 How to reach me mail
+
+<!---
+aman0650/aman0650 is a ✨ special ✨ repository because its `README.md` (this file) appears on your GitHub profile.
+You can click the Preview link to take a look at your changes.
+--->
